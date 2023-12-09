96174d8ccb459b7c558d9b8165660fbd60c5c4db
Alfredmus11 <alfredmushagalusam@gmail.com>
2023-12-09 20:08:43 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+const expresse = require("express")
+const ejs = require("ejs")
+const app = expresse()
+app.set("views", "./views")
+app.set("view engine", "ejs")
+app.listen(3000, function(){console.log("http://localhost:"+3000)
+console.log(process.env.NODE_ENV)})
+app.get("/", function(req, res){res.render("accueil")})
